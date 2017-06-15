a2bbabf7f17912e6b9d90862a6535a1da35f2184
cwklausing <cwklausing@gmail.com>
2017-06-15 12:44:18 -0500

    add 'See Process' button, more styling tweaks, hover effects

--- parts/home-second-fold.php
+++ parts/home-second-fold.php
@@ -2,7 +2,9 @@
 /**
  * Part for second fold of home page
  */
-$about_title = get_field( 'about_title' );
+$about_title 	= get_field( 'about_title' );
+$button_link 	= get_field( 'about_button' );
+$button_text	= get_field( 'about_button_text' );
 ?>
 
 <div class="fold-second">
@@ -13,6 +15,11 @@ $about_title = get_field( 'about_title' );
 		</div>
 		<div class="fold-second__content">
 			<?php the_content(); ?>
+		</div>
+		<div class="fold-second__button">
+			<a href="<?php echo esc_url( $button_link ); ?>" class="button button--view-process">
+				<?php echo esc_html( $button_text ); ?>
+			</a>
 		</div>
 	</div>
 </div>
