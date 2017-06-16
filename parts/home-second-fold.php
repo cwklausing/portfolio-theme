b9329a0815f29a40bbde0018a5602c3c1015ecd1
cwklausing <cwklausing@gmail.com>
2017-06-16 13:43:47 -0500

    make headers consistent on home page, add header mixin

--- parts/home-second-fold.php
+++ parts/home-second-fold.php
@@ -8,11 +8,11 @@ $button_text	= get_field( 'about_button_text' );
 ?>
 
 <div class="fold-second">
+	<div class="fold-second__header">
+		<h2 class="fold-second__title"><?php echo esc_html( $about_title ); ?></h2>
+		<div class="fold-second__underline sbc-underline"></div>
+	</div>
 	<div class="fold-second__text max-width">
-		<div class="fold-second__header">
-			<h2 class="fold-second__title"><?php echo esc_html( $about_title ); ?></h2>
-			<div class="fold-second__underline sbc-underline"></div>
-		</div>
 		<div class="fold-second__content">
 			<?php the_content(); ?>
 		</div>
